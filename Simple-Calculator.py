3283cf5df3d83cfc387e5acc970e59da42d3a226
Abhay-a12 <125866642+Abhay-a12@users.noreply.github.com>
2023-02-20 14:26:44 +0530

    Add files via upload

---
+++ Simple-Calculator.py
@@ -0,0 +1,13 @@
+a = int(input("first number :"))
+b = int(input("second number :"))
+
+
+print("************SIMPLE CALCULATOR************")
+
+print("Addition of", a ,"and", b, "is :" , a + b)
+print("Substraction of", a , "and", b, "is :", a - b)
+print("Multiplication of", a , "and", b , "is :", a*b)
+print("Division of", a , "and", b , "is :", a/b)
+print("Exponential of", a , "and", b , "is :", a**b)
+print("Floor Division of", a , "and", b , "is :", a//b)
+print("Modulus of", a , "and", b , "is :", a%b)
